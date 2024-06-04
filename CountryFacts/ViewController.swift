c3da5b463d0f8288f4882c14e370d72b0450fdf0
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-04 19:02:13 -0300

    Added alert on the home controller

--- CountryFacts/ViewController.swift
+++ CountryFacts/ViewController.swift
@@ -11,8 +11,40 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavBar()
+    }
+    
+    func setupNavBar() {
         title = "CountryFacts"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let infoButton = UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .plain, target: self, action: #selector(infoTapped))
+        navigationItem.rightBarButtonItem = infoButton
+    }
+    
+    @objc func infoTapped() {
+        let alertWidth: CGFloat = 250
+        let alertHeight: CGFloat = 200
+        let imageSize: CGFloat = 60
+        let imageHPosition = (alertWidth / 2) - (imageSize / 2)
+        let imageVPosition = 82
+        
+        let showAlert = UIAlertController(title: "CountryFacts", message: "An app to display facts about South American countries.", preferredStyle: .alert)
+        let imageView = UIImageView(frame: CGRect(x: imageHPosition, y: 82, width: imageSize, height: imageSize))
+        
+        imageView.image = UIImage(systemName: "globe.desk", withConfiguration: UIImage.SymbolConfiguration(pointSize: 16, weight: .light))
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .label
+        showAlert.view.addSubview(imageView)
+        
+        let height = NSLayoutConstraint(item: showAlert.view as Any, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: alertHeight)
+        let width = NSLayoutConstraint(item: showAlert.view as Any, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: alertWidth)
+        
+        showAlert.view.addConstraint(height)
+        showAlert.view.addConstraint(width)
+        
+        showAlert.addAction(UIAlertAction(title: "Got it!", style: .default))
+        self.present(showAlert, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
