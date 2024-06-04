a053f10d4dd8b48ea0a9be72c3d47efd03f986f8
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-06-04 11:34:25 -0300

    Initial Commit

---
+++ CountryFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CountryFacts
+//
+//  Created by Rodrigo Cavalcanti on 04/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
